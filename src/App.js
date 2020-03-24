cb812e30de9924475d4b15aebd8b66a4c424b809
Arya Shrivastava <62295393+arya-shrivastava@users.noreply.github.com>
2020-03-24 19:23:31 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import React from "react";
 // import Form from "./Form";
 // import FormContainer from "../src/Components/Form/component/FormContainer";
 import APIContainer from "./Components/API/component/APIContainer";
+import Details from "./Components/API/component/Details";
+import Nav from "./Components/API/component/nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      {/* From redux */}
-      {/* <FormContainer /> */}
-      <APIContainer />
-      {/* From react */}
-      {/* <Clock /> */}
-      {/* <Form /> */}
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/details" component={Details} />
+          <Route path="/apidata" component={APIContainer} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
 export default App;
