b37f2ad42c3dff3055568f59ddba41e30c94a9a6
Arya Shrivastava <62295393+arya-shrivastava@users.noreply.github.com>
2020-04-02 19:46:38 +0530

    02.04.2020

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
 import formReducer from "../src/Components/Form/reducer";
 import apiDataReducer from "../src/Components/API/reducer";
+import studentApiDataReducer from "../src/Components/StudentAPI/reducer";
 
-export default combineReducers({ users: formReducer, apiData: apiDataReducer });
+export default combineReducers({
+  users: formReducer,
+  apiData: apiDataReducer,
+  studentApiData: studentApiDataReducer
+});
